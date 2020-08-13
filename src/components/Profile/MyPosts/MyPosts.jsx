239087a7f151c1f9b6e0857a892a6b4d11d6e2c0
Anessy <n.repekha@gmail.com>
2020-08-13 11:47:54 +0300

    add new className in App.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,23 +1,23 @@
-import React, { component } from 'react';
-import  Post from './Post/Post'
+import React, {component} from 'react';
+import Post from './Post/Post'
 import classes from './MyPosts.module.css';
 
 
 const MyPosts = () => {
-return (
-<div>
+    return (
+        <div>
 
-  <div className={classes.content}>
-    my posts
+            <div className={classes.content}>
+                my posts
 
-  <div>
-    New posts
-  </div>
-    <Post message='It is my second post' count='6'/>
-    <Post message='It is my first post' count='7'/>
-</div>
-</div>
-)
+                <div>
+                    New posts
+                </div>
+                <Post message='It is my second post' count='6'/>
+                <Post message='It is my first post' count='7'/>
+            </div>
+        </div>
+    )
 }
 
 export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,8 @@ const Post = (props) => {
     return (
         <div>
             <div className={classes.content}>
-                <img src="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"></img>
+                <img
+                    src="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"></img>
                 <span>{props.message}</span>
                 <div>Like {props.count}</div>
             </div>
